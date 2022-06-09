dcefcfcd9a5721158e87595ee7be531aee107651
Nathan Reilly <nreilly@redhat.com>
2022-06-09 16:31:39 +1000

    Create roles and rolebindings

--- setup.sh
+++ setup.sh
@@ -22,3 +22,34 @@ until oc wait --for=jsonpath='{.status.server}'=Running argocd/openshift-gitops 
 done
 
 oc create -f argocd/applications/OpenShift-ServiceMesh/config.yaml
+
+
+# # {"apiGroups": ["machine.openshift.io"], "resources": ["*"], "verbs":["*"]},
+# 
+# 
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRole
+# metadata:
+#   labels:
+#   name: openshift-gitops-openshift-gitops-argocd-application-controller-miastra
+# rules:
+# - apiGroups:
+#   - 'miastra.io'
+#   resources:
+#   - '*'
+#   verbs:
+#   - '*'
+# 
+# 
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: openshift-gitops-openshift-gitops-argocd-application-controller-miastra
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: ClusterRole
+#   name: openshift-gitops-openshift-gitops-argocd-application-controller-miastra
+# subjects:
+# - kind: ServiceAccount
+#   name: openshift-gitops-argocd-application-controller
+#   namespace: openshift-gitops
