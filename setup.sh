99a0e5c6da3273cb38b9ff0cc88b9e05aae449c5
Nathan Reilly <nreilly@redhat.com>
2022-06-12 13:49:49 +1000

    Refactoring

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ until oc wait --for=jsonpath='{.status.server}'=Running argocd/openshift-gitops 
   sleep 10
 done
 
-oc create -f argocd/applications/openshift-bundle/config.yaml
+oc create -f argocd/applications/servicemesh-bundle/config.yaml
 
 
 # # {"apiGroups": ["machine.openshift.io"], "resources": ["*"], "verbs":["*"]},
