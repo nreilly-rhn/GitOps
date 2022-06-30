3908282716e69ea96ebce197f3b7c9013f3b3006
Nathan Reilly <nreilly@redhat.com>
2022-06-30 10:26:14 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ spec:
   printf "\n"
 fi
 
-printf "\rWaiting for default ArgoCD Instance to start"
+printf "\rWaiting for default ArgoCD Instance to start."
 until oc wait --for=jsonpath='{.status.server}'=Running argocd/openshift-gitops -n openshift-gitops &> /dev/null; do
   for ((i = 0; i < 4; ++i)); do
       printf .
@@ -28,7 +28,7 @@ done
 printf "\n"
 printf "\rDefault ArgoCD Instance started\n"
 
-printf "Waiting for ArgoCD Instance patch"
+printf "Waiting for ArgoCD Instance patch."
 until oc patch argocd -n openshift-gitops openshift-gitops --type merge -p='{"spec":{"resourceCustomizations":"argoproj.io/Application:\n  health.lua: |\n    hs = {}\n    hs.status = \"Progressing\"\n    hs.message = \"\"\n    if obj.status ~= nil then\n      if obj.status.health ~= nil then\n        hs.status = obj.status.health.status\n        hs.message = obj.status.health.message\n      end\n    end\n    return hs\noperators.coreos.com/Subscription:\n  health.lua: |\n    hs = {}\n    if obj.status ~= nil then\n      if obj.status.currentCSV ~= nil and (obj.status.state == \"AtLatestKnown\" or obj.status.state == \"UpgradeAvailable\" or obj.status.state == \"UpgradePending\") then\n        hs.status = \"Healthy\"\n        hs.message = \"Subcription installed\"\n        return hs\n      end\n    end\n    hs.status = \"Progressing\"\n    hs.message = \"Waiting for Subscription to complete.\"\n    return hs"}}' &> /dev/null; do
   for ((i = 0; i < 4; ++i)); do
       printf .
@@ -40,7 +40,7 @@ done
 printf "\n"
 printf "\rArgoCD Instance patch applied\n"
 
-#oc create -f ArgoCD/Infra/GitOps/config.yaml
+oc create -f ArgoCD/bootstrap/config.yaml
 
 # # {"apiGroups": ["machine.openshift.io"], "resources": ["*"], "verbs":["*"]},
 # 
